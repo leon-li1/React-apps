ba91333280244a9a986a0a337eaecfbf180f2311
Leon Li <leonli369@gmail.com>
2020-08-24 12:24:17 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -8,15 +8,11 @@ function App() {
 
   const addUser = () => {
     setCount((prevCount) => prevCount + 1);
-    if (count === 0) users.push({ id: 1, name: "Abby", message: "test" });
-    else if (count === 1) users.push({ id: 2, name: "Bob", message: "test2" });
-    else if (count === 2) users.push({ id: 3, name: "Carl", message: "test3" });
-    else
-      users.push({
-        id: count + 1,
-        name: "test_name" + (count + 1),
-        message: "test" + (count + 1),
-      });
+    users.push({
+      id: count + 1,
+      name: "test_name" + (count + 1),
+      message: "test" + (count + 1),
+    });
     setUsers(users);
   };
 
--- src/comment.js
+++ src/comment.js
@@ -2,16 +2,19 @@ import React from "react";
 
 function Comment({ name, message, handleDelete }) {
   return (
-    <div>
-      <button
-        className="btn btn-secondary btn-danger m-2"
-        onClick={handleDelete}
-        // style={{ height: 20, width: 20 }}
-      >
-        X
-      </button>
+    <div
+      style={{
+        border: "2px solid blue",
+        borderRadius: 5,
+        margin: 1,
+        display: "flex",
+      }}
+    >
       <h3>{name}</h3>
       <p>{message}</p>
+      <button className="btn btn-danger m-2 btn-xs" onClick={handleDelete}>
+        X
+      </button>
     </div>
   );
 }
